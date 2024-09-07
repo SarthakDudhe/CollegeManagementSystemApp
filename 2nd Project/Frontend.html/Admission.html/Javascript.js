6d40b9214c4d1f037c9b1f588a21095d1b0e7781
Shubham <dubeytech9619@gmail.com>
2024-09-07 21:58:04 +0530

    Add files via upload

---
+++ 2nd Project/Frontend.html/Admission.html/Javascript.js
@@ -0,0 +1,6 @@
+document.getElementById('admissionForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Simple validation can be done here
+    alert('Form submitted successfully!');
+});
---
+++ 2nd Project/UsingCpp/WindowApp.cpp
@@ -0,0 +1,64 @@
+#include <windows.h>
+
+// Function to process messages sent to the window
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Define and register the window class
+    const char* CLASS_NAME = "Sample Window Class";
+    
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc; // Function to process messages
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,                           // Optional window styles
+        CLASS_NAME,                  // Window class
+        "Learn to Program Windows",   // Window title
+        WS_OVERLAPPEDWINDOW,          // Window style
+
+        // Position and size of the window
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+
+    
+
+
+
+
